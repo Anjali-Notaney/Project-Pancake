6a6aa8936722c1ab8b5caffe00e3d7206c5d07ed
Anjali Notaney <anjalinotaney@hotmail.com>
2019-09-25 10:52:04 -0400

    Updates

---
+++ precache-manifest.bfe4ee3b3c975a1de1799e965226ed14.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde51814b857634929b647b912e7d2a7",
+    "url": "/WhatShouldIWatch/index.html"
+  },
+  {
+    "revision": "027ae31616ef42e3f400",
+    "url": "/WhatShouldIWatch/static/css/main.dd964f4b.chunk.css"
+  },
+  {
+    "revision": "c862998a6cc37bf513c3",
+    "url": "/WhatShouldIWatch/static/js/2.b6acd7cb.chunk.js"
+  },
+  {
+    "revision": "027ae31616ef42e3f400",
+    "url": "/WhatShouldIWatch/static/js/main.2b428023.chunk.js"
+  },
+  {
+    "revision": "1466facb91679da97050",
+    "url": "/WhatShouldIWatch/static/js/runtime~main.959343b3.js"
+  },
+  {
+    "revision": "77df44521001245755704a479bce1006",
+    "url": "/WhatShouldIWatch/static/media/BigMoviePoster.77df4452.png"
+  },
+  {
+    "revision": "abef1870a35266a9d9b6f9cea7e42c23",
+    "url": "/WhatShouldIWatch/static/media/Likes.abef1870.svg"
+  },
+  {
+    "revision": "37dbe149bac28fe2754c9c1c629c54f2",
+    "url": "/WhatShouldIWatch/static/media/MoviePoster.37dbe149.jpg"
+  },
+  {
+    "revision": "ccb2eda53fdb771dc5824610795c8f14",
+    "url": "/WhatShouldIWatch/static/media/Settings.ccb2eda5.svg"
+  },
+  {
+    "revision": "6c7b6c3154fdd5c277b7a7d1bc549101",
+    "url": "/WhatShouldIWatch/static/media/Unlike.6c7b6c31.svg"
+  },
+  {
+    "revision": "8a5a3ef59421c40268c64c7d6914f667",
+    "url": "/WhatShouldIWatch/static/media/Views.8a5a3ef5.svg"
+  },
+  {
+    "revision": "bd9601884804f5e1e39d436bc05b9f63",
+    "url": "/WhatShouldIWatch/static/media/blackPanther.bd960188.jpg"
+  },
+  {
+    "revision": "aca62c71a0ca907ff2240886ff3ddfbb",
+    "url": "/WhatShouldIWatch/static/media/breakingbad.aca62c71.jpg"
+  },
+  {
+    "revision": "cfb2fe79623d78c1ef86b8221c9e41c8",
+    "url": "/WhatShouldIWatch/static/media/chevron.cfb2fe79.svg"
+  },
+  {
+    "revision": "e4f8301f0bb6f1370f70c018b9d163f4",
+    "url": "/WhatShouldIWatch/static/media/chevronLeft.e4f8301f.svg"
+  },
+  {
+    "revision": "a3d1eb4df1535c4082fbbf4bcb476ee7",
+    "url": "/WhatShouldIWatch/static/media/firetruck.a3d1eb4d.jpg"
+  },
+  {
+    "revision": "dccc29984d21589db0ec5b10df29a395",
+    "url": "/WhatShouldIWatch/static/media/mahomes.dccc2998.jpg"
+  },
+  {
+    "revision": "6dcb00e37e3394d0483f451203a64e8c",
+    "url": "/WhatShouldIWatch/static/media/moonlight.6dcb00e3.jpg"
+  },
+  {
+    "revision": "f07adf7c931023d9eeb0f06ca232cf52",
+    "url": "/WhatShouldIWatch/static/media/natalie-portman.f07adf7c.jpg"
+  },
+  {
+    "revision": "9788d242fccf509de39144e4ab01da13",
+    "url": "/WhatShouldIWatch/static/media/plus.9788d242.svg"
+  },
+  {
+    "revision": "1d949708ddbf2c6cd856e419aed17d06",
+    "url": "/WhatShouldIWatch/static/media/poster.1d949708.jpg"
+  },
+  {
+    "revision": "912f62dd97274e04296e114e350b6e11",
+    "url": "/WhatShouldIWatch/static/media/puppy.912f62dd.jpg"
+  },
+  {
+    "revision": "d983705efe097541dc70f1492533a11e",
+    "url": "/WhatShouldIWatch/static/media/rdj.d983705e.jpg"
+  },
+  {
+    "revision": "1fa83a07af3c57d4fa70da21e7498794",
+    "url": "/WhatShouldIWatch/static/media/search.1fa83a07.svg"
+  }
+]);
